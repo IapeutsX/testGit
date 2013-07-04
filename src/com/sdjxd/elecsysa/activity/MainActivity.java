f28cd56c01f490cd603e95b6a92470b0ca478f54
IapeutsX <545658784@qq.com>
2013-07-04 17:23:41 +0800

    init

---
+++ src/com/sdjxd/elecsysa/activity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.sdjxd.elecsysa.activity;
+
+import com.sdjxd.elecsys.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity 
+{
+	private ImageView img=null;
+	private TextView usn=null,pwd=null;
+	private EditText editUsn=null,editPwd=null;
+	private Button login=null,setting=null;
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		findById();
+		login.setText(R.string.login);
+		setting.setText(R.string.setting);
+	}
+	private void findById()
+	{
+		img=(ImageView) findViewById(R.id.opening_img);
+		usn=(TextView) findViewById(R.id.usn);
+		pwd=(TextView) findViewById(R.id.pwd);
+		editUsn=(EditText) findViewById(R.id.edit_usn);
+		editPwd=(EditText) findViewById(R.id.edit_pwd);
+		login=(Button) findViewById(R.id.login);
+		setting=(Button) findViewById(R.id.setting);
+	}
+}
